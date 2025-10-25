3210b60d57e49e2e0ed6df7f68397b9812db6425
NandhuReddy007 <spandhanreddy1001@gmail.com>
2025-10-25 12:22:36 +0530

    Create tb_pkg package with necessary includes

---
+++ package.sv
@@ -0,0 +1,20 @@
+package tb_pkg;
+// import uvm_pkg::*;
+ // `include "uvm_macros.svh"
+`include "packet.sv"
+`include "transaction.sv"
+`include "axi_sequence.sv"
+`include "apb_sequence.sv"
+`include "axi_driver.sv"
+`include "apb_driver.sv"
+`include "axi_monitor.sv"
+`include "apb_monitor.sv"
+`include "scoreboard.sv"
+`include "axi_agent.sv"
+`include "apb_agent.sv"
+`include "environment.sv"
+//`include "test_collection.sv"
+//`include "test.sv"
+
+
+endpackage
